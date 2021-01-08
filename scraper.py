1bd3ddd46e18636d1bb34bdc91f68c7d22b94302
radoslaw.jeruzal <radoslaw.jeruzal@xcaliber.com>
2021-01-08 23:49:09 +0100

    implement postgres on conflict update

--- scraper.py
+++ scraper.py
@@ -2,10 +2,28 @@ import baker
 import numpy as np
 from binance.client import Client
 from config.base import api_key, api_secret
-from utils.session import session_scope
-from utils.dataframe_manipulation import binanceDataFrame
+from sqlalchemy.types import DateTime, Numeric
+from utils.session import session_scope, engine
+from utils.dataframe_manipulation import np_array_to_ohlc_df
 
 client = Client(api_key, api_secret)
+
+def replace_into(table, conn, keys, data_iter):
+    from sqlalchemy.dialects.postgresql import insert
+
+    data = [dict(zip(keys, row)) for row in data_iter]
+
+    stmt = insert(table.table).values(data)
+    print(stmt.excluded)
+    #stmt.on_isertpostgresql_insert_on_conflict()
+    stmt = stmt.on_conflict_do_update(index_elements=['ts'],
+                                             set_=dict(open=stmt.excluded.open,
+                                                       close=stmt.excluded.close,
+                                                       high=stmt.excluded.high,
+                                                       low=stmt.excluded.low,
+                                                       volume=stmt.excluded.volume))
+
+    conn.execute(stmt)
 
 @baker.command
 def run(symbol='ETHEUR'):
@@ -15,7 +33,35 @@ def run(symbol='ETHEUR'):
         )
         # remove everything besides Time+OHLC+Volume
         klines = np.delete(klines, np.s_[6:12], 1)
-        # cast to dataframes
-        print(binanceDataFrame(klines))
+
+        # cast to dataframe
+        df = np_array_to_ohlc_df(klines)
+
+        print(df.index)
+        print(df)
+        # save to db
+
+        table_name = symbol.lower()
+
+        result =df.to_sql(
+            table_name,
+            engine,
+            if_exists='append',
+            index=False,
+            chunksize=500,
+            dtype={
+                "ts": DateTime,
+                "open": Numeric,
+                "high": Numeric,
+                "low": Numeric,
+                "close": Numeric,
+                "volume": Numeric
+            },
+            method=replace_into
+        )
+
+        print(result)
+
+
 
 baker.run()
--- utils/dataframe_manipulation.py
+++ utils/dataframe_manipulation.py
@@ -1,13 +1,16 @@
+import numpy as np
 import pandas as pd
-def binanceDataFrame(klines):
-    df = pd.DataFrame(klines.reshape(-1,6),dtype=float, columns = ('Open Time',
-                                                                    'Open',
-                                                                    'High',
-                                                                    'Low',
-                                                                    'Close',
-                                                                    'Volume'))
 
-    df['Open Time'] = pd.to_datetime(df['Open Time'], unit='ms')
+def np_array_to_ohlc_df(klines: np.core.multiarray) -> pd.DataFrame:
+    df = pd.DataFrame(klines.reshape(-1,6),dtype=float, columns = ('ts',
+                                                                    'open',
+                                                                    'high',
+                                                                    'low',
+                                                                    'close',
+                                                                    'volume'))
 
+
+    df['ts'] = pd.to_datetime(df['ts'], unit='ms')
+    #df.set_index('Open Time')
     return df
 
